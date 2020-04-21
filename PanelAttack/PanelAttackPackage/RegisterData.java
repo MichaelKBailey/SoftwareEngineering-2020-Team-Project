70c07f988268a77204e86d5a4a2db677bdd536df
jflooch <jfluech2@cub.uca.edu>
2020-04-20 23:34:34 -0500

    RegisterData finished

--- PanelAttack/PanelAttackPackage/RegisterData.java
+++ PanelAttack/PanelAttackPackage/RegisterData.java
@@ -1,6 +1,38 @@
 package PanelAttackPackage;
 
 public class RegisterData {
-//(˵ ͡⚆ ͜ʖ ͡⚆˵)
-	//another test
+	public String username;
+	public String password;
+	public String passwordVerify;
+	
+	
+	public RegisterData(String username, String password, String passwordVerify) {
+		this.username = username;
+		this.password = password;
+		this.passwordVerify = passwordVerify;
+	}
+	
+	public void setPasswordVerify(String passwordVerify) {
+		this.passwordVerify = passwordVerify;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPasswordVerify() {
+		return this.passwordVerify;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
 }
