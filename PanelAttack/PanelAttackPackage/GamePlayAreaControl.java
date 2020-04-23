ea4cd65bd81459dd1117b162981be83a6fa3526e
jflooch <jfluech2@cub.uca.edu>
2020-04-22 19:42:01 -0500

    Created client side GameArea controller

--- PanelAttack/PanelAttackPackage/GamePlayAreaControl.java
+++ PanelAttack/PanelAttackPackage/GamePlayAreaControl.java
@@ -1,5 +1,40 @@
 package PanelAttackPackage;
 
-public class GamePlayAreaControl {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JPanel;
+
+public class GamePlayAreaControl implements ActionListener {
+
+	private JPanel panel;
+	private GameClient client;
+	private JPanel cards;
+	private JPanel PlayerOnePanel, PlayerTwoPanel;
+
+	public GamePlayAreaControl(JPanel panel, GameClient client) {
+		this.panel = panel;
+		this.client = client;
+	}
+	
+	public void gameStart() {
+		this.PlayerOnePanel.setVisible(true);
+		this.PlayerTwoPanel.setVisible(true);
+	}
+	
+	/*
+	 * public void setCards(JPanel cards) {
+		this.cards = cards;
+	}
+	*/
+
+	public void actionPerformed(ActionEvent ae) {
+		String command = ae.getActionCommand();
+	}
+
+	public void drawBoard(Board b) {
+		////////////////////////////////
+		//SHOULD THIS
+		////////////////////////////////
+	}
 }
