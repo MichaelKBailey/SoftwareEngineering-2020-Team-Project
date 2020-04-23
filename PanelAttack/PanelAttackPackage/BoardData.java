9e532a40861cadd4f69048aa284d6f6d708951fd
jflooch <jfluech2@cub.uca.edu>
2020-04-23 00:28:03 -0500

    GameData was divided into Game and BoardData

---
+++ PanelAttack/PanelAttackPackage/BoardData.java
@@ -0,0 +1,170 @@
+package PanelAttackPackage;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class BoardData
+{	//This class abstracts the precise pixel positioning of the button blocks into integer indices in a grid
+	private int x;		//topleft x coordinate of the boundaries for blocks
+	private int y;		//topleft y coordinate of the boundaries for blocks
+	private int width;	//width of boundaries for blocks
+	private int height;	//height of boundaries for blocks	
+	public ArrayList<ArrayList<Block>> grid;
+	
+	
+	public BoardData(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.grid = new ArrayList<ArrayList<Block>>();
+		 
+		//Generate the 6 rows of starting blocks:
+		for (int i = 0; i < 6; i++) {
+			generateNewRow(0);
+		}
+	}
+		
+public void generateTrash(ArrayList<ArrayList<Block>> grid, int width, int height, boolean right0left1) {
+	//Trash blocks either fill the GameData (6blocks wide) or are against the left or right side (4blocks wide)
+	
+	generateNewRow(grid.size());	//if the GameData has 5 rows, indices 0-4, then size() returns 5 which will be the index of the next row on top
+	for (Block b : grid.get(grid.size()-1)) {	//set the trash flag on the new row
+		b.setTrash(true);
+		b.setColor(Color.gray);
+	}
+	if (width == 4) {
+		if (right0left1 == true) {
+			grid.get(grid.size()).set(4, null);
+			grid.get(grid.size()).set(5, null);
+		}
+		else {
+			grid.get(grid.size()).set(0, null);
+			grid.get(grid.size()).set(1, null);
+		}
+	}
+}
+
+public void generateNewRow(int row_index) {
+	//row_index is 0 for a new row at the bottom/below the bottom
+	ArrayList<Block> newrow = new ArrayList<Block>();
+	int new_y = 0;
+	
+	//Is this a new row at the bottom, or trash blocks at the top?
+	if (row_index == 0) {
+		newrow.add(0, new Block(x, new_y));
+		newrow.add(1, new Block(x+30, new_y));
+		newrow.add(2, new Block(x+60, new_y));
+		newrow.add(3, new Block(x+90, new_y));
+		newrow.add(4, new Block(x+120, new_y));
+		newrow.add(5, new Block(x+150, new_y));
+		grid.add(0, newrow);
+	}
+	else {
+		//Find a non-null block in the topmost row to get its y coord, then add 40 for the new row's y
+		for (Block b : grid.get(grid.size()-1)) {
+			if (b != null) {
+				new_y = b.getCurrentY() + 40;
+				break;
+			}
+		}
+
+		newrow.add(0, new Block(x, new_y));
+		newrow.add(1, new Block(x+30, new_y));
+		newrow.add(2, new Block(x+60, new_y));
+		newrow.add(3, new Block(x+90, new_y));
+		newrow.add(4, new Block(x+120, new_y));
+		newrow.add(5, new Block(x+150, new_y));
+		grid.add(grid.size(), newrow);
+	}
+	
+	//Generate the colors for the new row
+	for (Block b : newrow) {
+		b.setColor(randomColor());
+	}
+}
+
+public boolean changeBlockPosition (int x1, int y1, int x2, int y2) {
+	//Returns false if block at either position is null;
+	//if (grid.get(y1))
+	return true;
+}
+
+public boolean advanceGrid() {
+	/*It takes 15 ticks upward until a new row is playable,
+	 * therefore this method ticks a row (and the whole GameData) upward by 1/15th of a block's height,
+	 * by traversing every block and ticking their y values.
+	 * Speed increases means this is called more frequently, but the actual change in height per tick remains the same
+	 * Returning false means game over
+	*/
+	
+	for (ArrayList<Block>row : grid) {
+		for (Block block : row) {
+			block.setCurrentY((block.getCurrentY() + 2));
+		}
+	}
+	
+	//Check if the last row's y coordinate is above the panel height, in which case GAME OVER:
+		for (Block b : grid.get(grid.size()-1)) {
+			if (b != null) {
+				if (grid.get(grid.size()-1).get(0).getCurrentY() > y) {
+					return false;
+				}
+				else {
+					return true;
+				}
+			}
+		}
+		return true;
+	}
+	
+	private Color randomColor() {
+		Color c = Color.red;
+		Random rand = new Random();
+		int n = rand.nextInt(5);
+		
+		switch(n) {
+			case 0:
+				c = Color.red;
+				break;
+			case 1:
+				c = Color.green;
+				break;
+			case 2:
+				c = Color.yellow;
+				break;
+			case 3:
+				c = Color.magenta;
+				break;
+			case 4:
+				c = Color.blue;
+				break;
+			default:
+				break;
+		}
+		
+	return c;
+	}
+	
+	public void processMove() {
+		//Check if the blocks can be switched:
+		/*
+		 * if (|| ) {
+			return;
+		}
+		else {		//They can be switched
+			
+			//Switch
+			//Check if needs to fall
+			checkForCombo
+			
+		}
+		*/
+	}
+	
+	public void checkForCombo() {
+		
+	}
+}
+
--- PanelAttack/PanelAttackPackage/Game.java
+++ PanelAttack/PanelAttackPackage/Game.java
@@ -5,18 +5,18 @@ public class Game
 {	private int speed; 		//Number of milliseconds per grid advance
 	private int ticknum;	//Number of milliseconds per speed up
 	private boolean paused;	//True if game hasnt started
-	private GameData gameData1;	//Game data for player 1
-	private GameData gameData2;	//Game data for player 2
 	public int difficulty;	//1,2,3 = easy, med, hard
+	public BoardData board1;	//Player 1's board
+	public BoardData board2;	//Player 2's board
+	
 	
 	public Game(int difficulty) {
-		
-		this.gameData1 = new GameData(30, 35, 194, 382);	//These are from GameLayoutManager player areas.
-		this.gameData2 = new GameData(477, 35, 194, 382);
 		setSpeed(15);
 		this.paused = true;
+		this.board1 = new BoardData(30, 35, 194, 382);
+		this.board2 = new BoardData(477, 35, 194, 382);
 	}
-
+		
 	public void gameStart() {
 		this.paused = false;
 	}
@@ -25,27 +25,9 @@ public class Game
 		if (this.ticknum == 15000) {
 			
 		}
-			gameData1.advanceGrid();
-			gameData2.advanceGrid();
-	}
-	
-	public void processMove() {
-		//Check if the blocks can be switched:
-		/*
-		 * if (|| ) {
-			return;
-		}
-		else {
-			
-		}
-		*/
-	}
-	
-	public void checkForCombo() {
-		
-	}
-	
-	
+			//gameData1.advanceGrid();
+			//gameData2.advanceGrid();
+	}	
 	
 	public void setSpeed(int speed) {
 		this.speed = speed;
@@ -53,21 +35,5 @@ public class Game
 	
 	public int getSpeed() {
 		return this.speed;
-	}
-	
-	public void setgameData1(GameData gameData) {
-		this.gameData1 = gameData;
-	}
-	
-	public GameData getgameData1() {
-		return this.gameData1;
-	}
-	
-	public void setgameData2(GameData gameData) {
-		this.gameData2 = gameData;
-	}
-	
-	public GameData getgameData2() {
-		return this.gameData2;
 	}
 }
