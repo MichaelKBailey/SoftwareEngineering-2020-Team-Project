4053de66a61b544daad46c1dc219abc31e0b7828
Regan <55265516+radiant-rsb@users.noreply.github.com>
2020-04-23 03:15:20 -0500

    LoginTest update 1

--- PanelAttack/Tests/LoginTest.java
+++ PanelAttack/Tests/LoginTest.java
@@ -1,11 +1,10 @@
 package Tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import PanelAttackPackage.*;
 
 public class LoginTest {
-
 	@Test
 	public void test() {
 		fail("Not yet implemented");
